4cf42e1004677d13d1d51676f05ca67336207664
yangalvin <649606409@qq.com>
2015-02-03 19:23:33 +0800

    这个是完整的

---
+++ src/main/java/alvin/learn/kafka/hello_kafka/Hello.java
@@ -0,0 +1,13 @@
+package alvin.learn.kafka.hello_kafka;
+
+/**
+ * Hello world!
+ *
+ */
+public class Hello
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
